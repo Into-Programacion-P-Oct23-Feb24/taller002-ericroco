582d2a26d0b343d249eec1903eb895ece18a4214
Erick Rodas <erickrodas559@gmail.com>
2023-10-21 14:27:21 -0500

    segundo commit

---
+++ Taller002ErickRodas/src/main/java/com/mycompany/taller002erickrodas/Taller002ErickRodas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.taller002erickrodas;
+
+/**
+ *
+ * @author StarMedia
+ */
+public class Taller002ErickRodas {
+
+    public static void main(String[] args) {
+        System.out.println("Ejemplo básico de Java Manejado desde GitHub Versionado a través de Git Manejado desde GitHub Creado por ericroco");
+    }
+}
