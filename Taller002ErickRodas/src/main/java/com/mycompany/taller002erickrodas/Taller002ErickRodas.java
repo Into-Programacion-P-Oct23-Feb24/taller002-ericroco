962288702cd68a42b01ea9d000b53af62ee5e0d2
Erick Rodas <erickrodas559@gmail.com>
2023-10-21 14:50:56 -0500

    segundo commit

--- Taller002ErickRodas/src/main/java/com/mycompany/taller002erickrodas/Taller002ErickRodas.java
+++ Taller002ErickRodas/src/main/java/com/mycompany/taller002erickrodas/Taller002ErickRodas.java
@@ -11,6 +11,9 @@ package com.mycompany.taller002erickrodas;
 public class Taller002ErickRodas {
 
     public static void main(String[] args) {
-        System.out.println("Ejemplo básico de Java Versionado a través de Git Manejado desde GitHub");
+        System.out.println("Ejemplo de Java\n" +
+"Manejado desde GitHub\n" +
+"Versionado a través de Git\n" +
+"Creado por ericroco");
     }
 }
