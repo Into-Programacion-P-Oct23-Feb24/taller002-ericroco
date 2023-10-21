2effeb56d2db81e2e5ff29526085f46a431c6c80
Erick Rodas <erickrodas559@gmail.com>
2023-10-21 14:56:51 -0500

    tercer commit, trabajando con Git y GitHubClassroom

---
+++ proyecto02/src/main/java/com/mycompany/proyecto02/Proyecto02.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto02;
+
+/**
+ *
+ * @author StarMedia
+ */
+public class Proyecto02 {
+
+    public static void main(String[] args) {
+        System.out.println("Ejemplo dos básico de Java\n" +
+"Versionado a través de Git\n" +
+"Manejado desde GitHub\n" +
+"Además se usar GitHubClassroom");
+    }
+}
